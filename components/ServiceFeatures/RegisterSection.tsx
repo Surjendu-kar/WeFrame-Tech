93dddf33449f94bc9fed05e7b1f652250ace6316
Surjendu kar <rahulgotp9988@gmail.com>
2024-12-10 02:25:49 +0530

    Add RegisterSection

---
+++ components/ServiceFeatures/RegisterSection.tsx
@@ -0,0 +1,85 @@
+import { Box, Stack, Typography, styled, TextField } from "@mui/material";
+import Image from "next/image";
+import Frame from "@/public/ServiceFeatureImg/Frame.svg";
+
+const RegisterContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  gap: theme.spacing(2),
+  padding: theme.spacing(6, 2),
+}));
+
+const ContentStack = styled(Stack)(({ theme }) => ({
+  padding: theme.spacing(3),
+  background: "#FFF3F9",
+  borderRadius: theme.spacing(3),
+}));
+
+const RegisterText = styled(Typography)(({ theme }) => ({
+  fontSize: theme.spacing(4.3),
+  color: theme.palette.grey[600],
+  marginBottom: theme.spacing(1),
+  "& span": {
+    color: theme.palette.primary.light,
+    fontSize: theme.spacing(4.3),
+  },
+}));
+
+const Description = styled(Typography)(({ theme }) => ({
+  fontSize: theme.spacing(1.4),
+  color: "#BDA2B0",
+  marginBottom: theme.spacing(3),
+}));
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  width: theme.spacing(56.5),
+  "& .MuiInputBase-root": {
+    height: theme.spacing(5.8),
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: "8px",
+  },
+}));
+
+const RegisterButton = styled("button")(({ theme }) => ({
+  backgroundColor: theme.palette.primary.light,
+  color: theme.palette.background.paper,
+  padding: theme.spacing(1.9, 1.6),
+  borderRadius: "8px",
+  textTransform: "uppercase",
+  border: "none",
+  "&:hover": {
+    backgroundColor: "#4bc0cb",
+  },
+}));
+
+const InputContainer = styled(Box)({
+  display: "flex",
+  gap: "16px",
+  alignItems: "center",
+});
+
+const RegisterSection = () => {
+  return (
+    <RegisterContainer>
+      <Image src={Frame} alt="frame img" />
+      <ContentStack>
+        <RegisterText>
+          S&apos;inscrire & économiser <span>10%</span>
+        </RegisterText>
+        <Description>
+          Office ipsum you must be muted. Synergize helicopter prioritize anyway
+          job due harvest most opportunity didn&apos;t. Yet busy any meeting
+          shark light marginalised 4-blocker message. Productize corporate nail
+          caught synergy highlights lunch. Company another pushback items dear
+          or any.
+        </Description>
+        <InputContainer>
+          <StyledTextField placeholder="john@doe.com" variant="outlined" />
+          <RegisterButton>S&apos;INSCRIRE</RegisterButton>
+        </InputContainer>
+      </ContentStack>
+    </RegisterContainer>
+  );
+};
+
+export default RegisterSection;
--- components/ServiceFeatures/ServiceFeatures.tsx
+++ components/ServiceFeatures/ServiceFeatures.tsx
@@ -5,23 +5,23 @@ import DeliveryImg from "@/public/ServiceFeatureImg/Groupe_25.svg";
 import OrderImg from "@/public/ServiceFeatureImg/Groupe_56.svg";
 import Image from "next/image";
 import theme from "@/theme";
+import RegisterSection from "./RegisterSection";
 
 const Container = styled(Stack)(({ theme }) => ({
   width: "100%",
   padding: theme.spacing(6, 0),
-  alignItems: "center",
-  backgroundColor: "#fff4f991",
+  background: "linear-gradient(180deg, #fff4f991 0%, #ffffff 100%)",
   gap: theme.spacing(5),
   marginTop: theme.spacing(6),
 }));
 
 const Title = styled(Typography)(({ theme }) => ({
-  fontSize: "35px",
+  fontSize: theme.spacing(3.5),
   color: theme.palette.grey[600],
+
   "& span": {
     color: "#5CD2DD",
-    fontFamily: "Cabinet Grotesk",
-    fontWeight: 800,
+    fontWeight: 700,
     fontSize: theme.spacing(3.5),
   },
 }));
@@ -68,29 +68,31 @@ const ServiceFeatures = () => {
 
       <FeaturesGrid>
         <FeatureItem>
-          <Image src={DeliveryImg} alt="" />
+          <Image src={DeliveryImg} alt="DeliveryImg" />
           <Typography className="title">Livraison & Reprise</Typography>
           <Typography className="subtitle">Selon vos besoins</Typography>
         </FeatureItem>
 
         <FeatureItem>
-          <Image src={CleaningImg} alt="" />
+          <Image src={CleaningImg} alt="CleaningImg" />
           <Typography className="title">Nettoyage</Typography>
           <Typography className="subtitle">Selon vos besoins</Typography>
         </FeatureItem>
 
         <FeatureItem>
-          <Image src={OrderImg} alt="" />
+          <Image src={OrderImg} alt="OrderImg" />
           <Typography className="title">Commande Illimitée</Typography>
           <Typography className="subtitle">Tout est possible</Typography>
         </FeatureItem>
 
         <FeatureItem>
-          <Image src={AmbulanceImg} alt="" />
+          <Image src={AmbulanceImg} alt="AmbulanceImg" />
           <Typography className="title">Transport & Enlèvement</Typography>
           <Typography className="subtitle">On s&apos;occupe de tout</Typography>
         </FeatureItem>
       </FeaturesGrid>
+
+      <RegisterSection />
     </Container>
   );
 };
